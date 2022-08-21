14035759b3696f5316dfb628ee51593e08d063a4
Герман Гриценко <gerfest.gricenko@gmail.com>
2022-08-21 22:00:13 +0300

    feat: quizz models

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,1 +1,2 @@
 from .user import User  # noqa
+from .quizz import RightAnswer, Question  # noqa
---
+++ app/db/models/quizz.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+from sqlalchemy.schema import Column
+from sqlmodel import ARRAY, Field, Relationship, SQLModel, String
+
+
+class Language(str, Enum):
+    PYTHON = "python"
+    GO = "go"
+    JAVASCRIPT = "javascript"
+    JAVA = "java"
+    RUBY = "ruby"
+    PHP = "php"
+
+
+class Difficulty(str, Enum):
+    JUNIOR = "junior"
+    MIDDLE = "middle"
+    SENIOR = "senior"
+
+
+class QuestionType(str, Enum):
+    MULTIPLE = "multiple"
+    SINGLE = "single"
+
+
+class Question(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    difficulty: Difficulty
+    language: Language
+    type: QuestionType
+    fake_answers: list[str] = Field(sa_column=Column(ARRAY(String)))
+    right_answers: list["RightAnswer"] = Relationship(back_populates="question")
+
+
+class RightAnswer(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    description: str
+    question_id: int = Field(default=None, foreign_key="question.id")
+    question: Question = Relationship(back_populates="right_answers")
